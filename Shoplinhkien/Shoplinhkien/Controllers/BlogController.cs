39f8be0e33f73f818087380f945b441b6b71bfde
luan306 <nguyenluan3062001@gmail.com>
2023-04-19 23:47:01 +0700

    v1

---
+++ Shoplinhkien/Shoplinhkien/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shoplinhkien.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Shoplinhkien/Shoplinhkien/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shoplinhkien.Models;
+
+namespace Shoplinhkien.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ShoplinkkienContext _context;
+        public ProductController(ShoplinkkienContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details(int id)
+        {
+            var product = _context.Products.Include(x => x.Cat).FirstOrDefault(x => x.ProductId == id);
+            if (product == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+    }
+}
